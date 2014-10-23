2cc360e485ca59e09b8ed4bb9fd66d187fbfeac7
charleszuo <charleszuo1985@gmail.com>
2014-10-23 14:18:32 +0800

    First commit

---
+++ src/com/zc/lock/Lock.java
@@ -0,0 +1,7 @@
+package com.zc.lock;
+
+public interface Lock {
+	public void lock();
+	
+	public void unlock();
+}
---
+++ src/com/zc/lock/Main.java
@@ -0,0 +1,31 @@
+package com.zc.lock;
+
+public class Main {
+	private static Lock lock = new TimeCost(new ArrayLock(150));
+	
+	//private static Lock lock = new CLHLock();
+	
+	//private static TimeCost timeCost = new TimeCost(new TTASLock());
+	
+	private static volatile int value = 0;
+	public static void method(){
+		lock.lock();
+		//System.out.println("Value: " + ++value);
+		lock.unlock();
+	}
+	
+	public static void main(String[] args) {
+		for(int i = 0; i < 100; i ++){
+			Thread t = new Thread(new Runnable(){
+	
+				@Override
+				public void run() {
+					method();
+				}
+				
+			});
+			t.start();
+		}
+	}
+
+}
